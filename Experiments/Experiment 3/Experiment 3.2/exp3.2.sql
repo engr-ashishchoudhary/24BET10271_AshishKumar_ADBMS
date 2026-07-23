d8b609685c9832d6388a8d4a81202bad5a0f4ce1
Ashish Kumar <288236538+Er-ashishkumar@users.noreply.github.com>
2026-07-23 10:22:01 +0530

    Update exp3.2.sql

--- Experiments/Experiment 3/Experiment 3.2/exp3.2.sql
+++ Experiments/Experiment 3/Experiment 3.2/exp3.2.sql
@@ -6,33 +6,79 @@ CREATE TABLE employees (
 );
 
 INSERT INTO employees (emp_id, emp_name, emp_salary, emp_city) VALUES
-(9, 'i', 80000.00, 'Mumbai')
-(1, 'A', 85000.00, 'Mumbai'),
-(2, 'B', 95000.00, 'Mumbai'),
-(3, 'C', 60000.00, 'Delhi'),
-(4, 'D', 110000.00, 'Bangalore'),
-(5, 'E', 55000.00, 'Delhi'),
-(6, 'F', 105000.00, 'Bangalore'),
-(7, 'G', 72000.00, 'Kolkata'),
-(8, 'h', 92000.00, 'Kolkata'),
-(9, 'i', 80000.00, 'Mumbai');
+(101, 'Amit Sharma', 85000.00, 'Mumbai'),
+(102, 'Priya Patel', 95000.00, 'Mumbai'),
+(103, 'Rahul Verma', 60000.00, 'Delhi'),
+(104, 'Ananya Iyer', 110000.00, 'Bangalore'),
+(105, 'Vikram Singh', 55000.00, 'Delhi'),
+(106, 'Sneha Reddy', 105000.00, 'Bangalore'),
+(107, 'Rohan Das', 72000.00, 'Kolkata')
 
-select emp_city, count (*) as cnt
-from employees
-group by emp_city
-having count (*) = 3;
+-- AGGREGATE FUNCTION -> SUM(),MIN(),MAX(),AVG(),COUNT(),
+-- DISTINCT
+-- HAVING
 
-SELECT emp_city,COUNT(*) as cnt
+-- FIND THE TOTAL NUMBER OF EMPLOYEES IN EACH CITY
+SELECT  emp_city ,count(*) as cnt FROM EMPLOYEES
+GROUP BY emp_city
+
+
+
+-- FIND THE TOTAL NUMBER OF EMPLOYEES IN EACH CITY AND SORT BY CNT IN ASC ORDER
+
+-- (i)
+SELECT  emp_city ,count(*) as cnt FROM EMPLOYEES
+GROUP BY emp_city
+ORDER BY CNT ASC
+-- (II)
+SELECT  emp_city ,count(EMP_ID) as cnt FROM EMPLOYEES
+GROUP BY emp_city
+ORDER BY CNT 
+
+-- (III)
+
+-- FIND THE NUMBER OF EMPLOYEES IN EACH CITY WHOSE SALARY IS GREATRE THAN EQUAL TO 90000
+
+-- 1.
+SELECT  emp_city , SUM(CASE WHEN EMP_SALARY>=90000 THEN 1 ELSE 0 END) AS CNT
 FROM EMPLOYEES
 GROUP BY emp_city
-ORDER BY emp_city;
+
+ORDER BY CNT DESC,EMP_CITY DESC
+
+-- 2.
+
+SELECT  emp_city , COUNT(CASE WHEN EMP_SALARY>=90000 THEN 1 END) AS CNT
+FROM EMPLOYEES
+GROUP BY emp_city
 
 
 
+-- FIND THE MAXIMUM EMPLOYEE SALARY IN EACH CITY
+
+SELECT  emp_city , MAX(EMP_SALARY) AS MAX_SALARY
+FROM EMPLOYEES
+GROUP BY emp_city
+
+-- FIND THE MINIMUM EMPLOYEE SALARY IN EACH CITY
+
+SELECT  emp_city , MIN(EMP_SALARY) AS MIN_SALARY
+FROM EMPLOYEES
+GROUP BY emp_city
+
+-- FIND ALL THH CITY WHOSE SALARY IS GRETARE TAHN EQUAL TO 85000
 
 
+SELECT  emp_city , MIN(EMP_SALARY) AS MIN_SALARY
+FROM EMPLOYEES
+GROUP BY emp_city
+HAVING  MIN(EMP_SALARY)>=85000
 
+-- 
+DISTINCT
 
-
-
+-- FIND THE DISTINCT CITY
+SELECT  DISTINCT EMP_CITY
+FROM EMPLOYEES
+ 
 
